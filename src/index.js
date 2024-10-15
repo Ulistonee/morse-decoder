0cd015fedf87999e4727b5a2c742f40f260069de
Ulistonee <ulistonee@gmail.com>
2024-10-15 21:36:54 +0500

    final

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodedMessage = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        const chunk = expr.slice(i, i + 10);
+
+        if (chunk === '**********') {
+            decodedMessage += ' ';
+        } else {
+            let morseSymbol = '';
+            for (let j = 0; j < chunk.length; j += 2) {
+                const binaryPair = chunk.slice(j, j + 2);
+                if (binaryPair === '10') {
+                    morseSymbol += '.';
+                } else if (binaryPair === '11') {
+                    morseSymbol += '-';
+                }
+            }
+            decodedMessage += MORSE_TABLE[morseSymbol];
+        }
+    }
+
+    return decodedMessage;
 }
 
 module.exports = {
